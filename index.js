b61daa233ca9e673bc6222711adacc40726c08f4
pramod <pramodkesarkar2222@gmail.com>
2023-07-31 15:24:40 +0530

    responsive changes

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const overlay = document.querySelector(".overlay");
 const closeButton = document.querySelector(".close-button");
 const arrow = document.getElementById("scrollArrow");
 const navbar = document.getElementById("navbar");
-
 const body = document.querySelector("body");
 const nav = document.querySelector("nav");
+const dateInputs = document.querySelectorAll('input[type="date"]');
+
+
+
+dateInputs.forEach(input => {
+  const placeholder = input.nextElementSibling;
+
+  input.addEventListener('input', () => {
+    placeholder.style.display = input.value ? 'none' : 'inline';
+  });
+});
+
 
 nav.style.transition = "all 0.5s ease-in-out";
 nav.style.display = "none";
@@ -48,4 +59,28 @@ closeButton.addEventListener("click", () => {
 
 arrow.addEventListener("click", function () {
   navbar.classList.add("navbar-fixed");
+});
+
+const menuIcon = document.getElementById('menu-icon');
+const sidebar = document.getElementById('sidebar');
+
+function isClickedInsideSidebar(event) {
+  return sidebar.contains(event.target) || menuIcon.contains(event.target);
+}
+
+menuIcon.addEventListener('click', () => {
+  sidebar.classList.toggle('hidden');
+});
+
+const listItems = document.querySelectorAll('.side-list-itom');
+listItems.forEach(item => {
+  item.addEventListener('click', () => {
+    sidebar.classList.add('hidden');
+  });
+});
+
+document.addEventListener('click', (event) => {
+  if (!isClickedInsideSidebar(event)) {
+    sidebar.classList.add('hidden');
+  }
 });
